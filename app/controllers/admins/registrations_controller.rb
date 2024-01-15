2e79798c2f873bc7135adf8eb1a3d6b92679e3d8
seenjey07 <chrosales07@gmail.com>
2024-01-16 00:36:33 +0800

    Fix admin and user navbar routing. Update navbar texts. Fix admins/registrations_ controller. Create Pending Sign-ups and Users Table.

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -1,9 +1,60 @@
 class Admins::RegistrationsController < ApplicationController
-  def create
-    super do |user|
-      user.admin_approved = false
-      user.save
-      UserMailer.approval_email(user).deliver_later
-    end
+  before_action :authenticate_admin!
+  
+  def new
+    super
   end
+
+  def create
+    super
+  end
+
+  def edit
+    super
+  end
+
+  def update
+    super
+  end
+
+  def destroy
+    super
+  end
+
+    def new_user
+     super do |user|
+       user.admin_approved = false
+       user.save
+     end
+   end
+
+   def create_user
+     super do |user|
+       user.admin_approved = false
+       user.save
+       UserMailer.approval_email(user).deliver_later
+     end
+   end
+
+   def show_user
+     super
+   end
+
+   def edit_user
+     super do |user|
+       user.admin_approved = true
+       user.save
+     end
+   end
+
+   def update_user
+     super do |user|
+       user.admin_approved = true
+       user.save
+     end
+   end
+
+   def destroy
+     super
+   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include Devise::Controllers::Helpers
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admins_path
+    else
+      super
+    end
+  end
 end
