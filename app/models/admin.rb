aac844474766d2344b32892a0a91edb9b539e537
seenjey07 <chrosales07@gmail.com>
2024-01-17 22:37:50 +0800

    Fix issues on sign-up approval. Finalize UI. Modify routing.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,6 +4,12 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :first_name, :last_name, :user_id, presence: true
+         validates :first_name, presence: true
+         validates :last_name, presence: true
+         validates :user_id, presence: true, uniqueness: true
+
+    def sign_up_params
+      params.require(:admin).permit(:first_name, :last_name, :user_id, :email, :password, :confirm_password)
+    end
 
 end
